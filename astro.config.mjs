0f16126f2172d50bed6a4f363056f67d4d33e71b
psmyrdek <przemek.smyrdek@gmail.com>
2024-12-18 12:42:06 +0100

    chore: simplify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
   adapter: node({
     mode: "standalone",
   }),
