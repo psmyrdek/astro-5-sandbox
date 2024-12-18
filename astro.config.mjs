e24258c9559ac0359ddfa4b6c2bb82d9e1899330
psmyrdek <przemek.smyrdek@gmail.com>
2024-12-18 19:00:28 +0100

    feat: use netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,12 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify(),
   server: {
     port: 3000,
   },
